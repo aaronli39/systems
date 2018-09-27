ae59f90957c0074be4e7fd2cdb62f734a07c9d9e
Aaron Li <aaronli39@gmail.com>
2018-09-26 22:09:09 -0400

    added demo files

--- demo/demo.c
+++ demo/demo.c
@@ -5,5 +5,14 @@ char *cat (char *out, const char *inp, int n) {
      int size = strlen(out);
      int i;
 
-     for (i = 0; i < size )
+     for (i = 0; i < n && inp[i] != "\0"; i++) {
+         out[size + i] = inp[i];
+     } return out;
+}
+
+int main() {
+    char s1[50] = "Hello ";
+    char *s2 = "world";
+    puts(cat(s1, s2, 2));
+    return 0;
 }
